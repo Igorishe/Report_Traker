8fe14824ed0147953b7dd25d660cd0fddab2b45f
Igor <admin@example.io>
2021-07-21 16:54:48 +0600

    burning report creation

--- telegram/services/ex_API_requests.py
+++ telegram/services/ex_API_requests.py
@@ -3,35 +3,13 @@ import os
 import requests
 from dotenv import load_dotenv
 
-from .parse_functions import parse_report
+from .format_functions import format_result, format_to_save
 
 load_dotenv()
 
 api_token = os.getenv('api_token')
 api_host = '127.0.0.1'
 api_port = '8000'
-
-def format_result(result):
-    """Format API response for telegram rendering"""
-    output = ''
-    for item in result:
-        text = item['text']
-        date = item['date']
-        status = item['status']
-        tag = item['tag']
-        author = item['author_name']
-        line = (
-            f'Кейс: {text}\n'
-            f'<code>Дата: {date}</code>\n'
-            f'Статус: <b>{status}</b>\n'
-            f'Тэг: <b>{tag}</b>\n'
-            f'Автор: {author}\n'
-            '\n'
-        )
-        output += line
-    if not output:
-        return 'Эта категория пуста'
-    return output
 
 
 def show_all(message, bot, filtered):
@@ -67,18 +45,7 @@ def show_all(message, bot, filtered):
 
 def report_save(message, bot):
     """Post new report to Traker base"""
-    text = message.text
-    reports = parse_report(text)
-    author_id = message.from_user.id
-    author_username = message.from_user.username
-    to_save = []
-    for report in reports:
-        obj_to_save = {
-            'author': author_id,
-            'author_name': author_username,
-            'text': report,
-        }
-        to_save.append(obj_to_save)
+    to_save = format_to_save(message)
     try:
         request = requests.post(
             url=f'http://{api_host}:{api_port}/api/v1/report/',
---
+++ telegram/services/format_functions.py
@@ -0,0 +1,44 @@
+from .parse_functions import parse_report
+
+
+def format_result(result):
+    """Format API response for telegram rendering"""
+    output = ''
+    for item in result:
+        text = item['text']
+        date = item['date']
+        status = item['status']
+        tag = item['tag']
+        author = item['author_name']
+        line = (
+            f'Кейс: {text}\n'
+            f'<code>Дата: {date}</code>\n'
+            f'Статус: <b>{status}</b>\n'
+            f'Тэг: <b>{tag}</b>\n'
+            f'Автор: {author}\n'
+            '________________________________\n'
+        )
+        output += line
+    if not output:
+        return 'Эта категория пуста'
+    return output
+
+
+def format_to_save(message):
+    """Serialize report objects for POST request"""
+    text = message.text
+    reports = parse_report(text)
+    author_id = message.from_user.id
+    author_username = message.from_user.username
+    to_save = []
+    for report in reports:
+        obj_to_save = {
+            'author': author_id,
+            'author_name': author_username,
+            'text': report,
+        }
+        if report.startswith('!!!'):
+            obj_to_save['text'] = report[3:]
+            obj_to_save['tag'] = 'Burning'
+        to_save.append(obj_to_save)
+    return to_save
